a087bcf75f603b03f5863380d3a9254cb3a89594
Sama Ahmed <26samahmed@csu.fullerton.edu>
2026-02-03 12:17:38 -0800

    add buttons to every page

--- src/routes/step2/+page.svelte
+++ src/routes/step2/+page.svelte
@@ -1,11 +1,17 @@
 <script>
   import Header from '$lib/components/header/Header.svelte';
 	import Footer from '$lib/components/footer/Footer.svelte';
+  import BackButton from '$lib/components/buttons/Back.svelte';
+  import NextButton from '$lib/components/buttons/Next.svelte';
 </script>
 
 
 <main class="bg-[#333745] min-h-screen p-6">
   <Header />
   <h1 class="font-aldrich text-4xl text-white text-center">Step 2: Choose the Layout</h1>
+  <div class="flex justify-center mt-10 gap-6">
+    <BackButton href="/step1" />
+    <NextButton href="/step3" />
+  </div>
   <Footer />
 </main>
--- src/routes/step4/+page.svelte
+++ src/routes/step4/+page.svelte
@@ -1,11 +1,17 @@
 <script>
   import Header from '$lib/components/header/Header.svelte';
 	import Footer from '$lib/components/footer/Footer.svelte';
+  import BackButton from '$lib/components/buttons/Back.svelte';
+  import NextButton from '$lib/components/buttons/Next.svelte';
 </script>
 
 
 <main class="bg-[#333745] min-h-screen p-6">
   <Header />
   <h1 class="font-aldrich text-4xl text-white text-center">Step 4: Choose Your Side</h1>
+  <div class="flex justify-center mt-10 gap-6">
+    <BackButton href="/step3" />
+    <NextButton href="/" />
+  </div>
   <Footer />
 </main>
