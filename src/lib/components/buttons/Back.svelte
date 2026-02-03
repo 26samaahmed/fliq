e4ee9f5813131906262b1a5fc7c3dae8759de806
Sama Ahmed <26samahmed@csu.fullerton.edu>
2026-02-02 22:51:25 -0800

    start implementing buttons

---
+++ src/lib/components/buttons/Back.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let href = '/';
+</script>
+
+<a
+  href={href}
+  class="bg-[#D9D9D9] hover:bg-white text-2xl font-aldrich hover:text-[#333745] py-1 px-6 border border-white hover:border-transparent rounded-full transition-colors duration-300"
+  >Back</a >
