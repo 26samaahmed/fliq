dbcec70f1727a394ed67048d661d8e32e71e7162
Sama Ahmed <26samahmed@csu.fullerton.edu>
2026-02-25 09:44:10 -0800

    work on sign up page (wip)

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -6,24 +6,21 @@
 <div class="bg-[#333745] min-h-screen flex flex-col p-6">
   <Header />
   <div class="text-center mt-20 flex-1">
-    <h1 class="font-aldrich text-4xl text-white">Join Us!</h1>
-    <p class="font-arimo text-xl text-[#E8F1F2] mt-5 w-230 mx-auto">[need to put text here] </p>
+    <h1 class="font-aldrich text-4xl text-white">Sign Up for Fliq.</h1>
+    <p class="font-arimo text-xl text-[#E8F1F2] mt-5 w-230 mx-auto">Create a free account or <span class="underline text-[#DCDFF5] cursor-pointer">log in</span>.</p>
 
     <form class="mt-10 flex flex-col items-center gap-6 text-white">
-      <div class="flex gap-6">
-        <input type="text" placeholder="First Name" class="w-64 px-4 py-2 rounded-md ring-2 ring-[#DCDFF5]" />
-        <input type="text" placeholder="Last Name" class="w-64 px-4 py-2 rounded-md ring-2 ring-[#DCDFF5]" />
-      </div>
+      <input type="text" placeholder="Name" class="w-72 px-4 py-2 rounded-md ring-2 ring-[#DCDFF5]" />
 
-      <div class="flex gap-6">
-        <input type="text" placeholder="Email" class="w-64 px-4 py-2 rounded-md ring-2 ring-[#DCDFF5]" />
-        <input type="text" placeholder="Phone Number" class="w-64 px-4 py-2 rounded-md ring-2 ring-[#DCDFF5]" />
-      </div>
+      <input type="text" placeholder="Email" class="w-72 px-4 py-2 rounded-md ring-2 ring-[#DCDFF5]" />
 
-      <div class="flex gap-6">
-        <input type="password" placeholder="Password" class="w-64 px-4 py-2 rounded-md ring-2 ring-[#DCDFF5]" />
-        <input type="password" placeholder="Confirm Password" class="w-64 px-4 py-2 rounded-md ring-2 ring-[#DCDFF5]" />
-      </div>
+      <input type="password" placeholder="Password" class="w-72 px-4 py-2 rounded-md ring-2 ring-[#DCDFF5]" />
+
+      <!-- Have option to sign in via google-->
+      <span class="text-[#E8F1F2]">OR</span>
+      <button class="text-[#E8F1F2] font-aldrich hover:bg-[#949FF2] transition duration-500">
+        Sign Up with Google
+      </button>
 
       <div class="flex items-center gap-2">
         <input type="checkbox" id="terms" class="w-5 h-5 text-[#DCDFF5] bg-gray-100 rounded border-gray-300 focus:ring-[#DCDFF5]">
