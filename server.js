c94383201d9064990d1c0cedd04c228a407638e6
kaillora <nathankai09@gmail.com>
2026-03-03 12:39:28 -0800

    updated file directory

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const io = new Server(server, {
 });
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static('src/lib/backend'))
 
 app.get('/', (req, res) => {
     res.redirect(`/${uuidV4()}`)
