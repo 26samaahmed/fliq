31e4f70bd8bf5b026e1f48db943eecbdb960dafb
Kate Lee <145916607+kaitlynhlee@users.noreply.github.com>
2026-04-09 03:23:53 -0700

    fixing step order with room creation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,7 @@
 <script>
-  import { goto } from '$app/navigation';
-  import { v4 as uuidV4 } from 'uuid';
   import landing_frame from '$lib/assets/landing-page-frame.svg';
   import Header from '$lib/components/header/Header.svelte';
   import Footer from '$lib/components/footer/Footer.svelte';
-
-  function startRoom() {
-    goto(`/room/${uuidV4()}`);
-  }
 </script>
 
 <main class="bg-[#333745] min-h-screen flex flex-col p-6">
@@ -20,11 +14,11 @@
         <i>Create and customize photo memories with your friends in real time as fliq. brings the photo booth experience to you wherever you are.</i>
       </p>
       
-      <button
-        on:click={startRoom}
+      <a
+        href="/step1"
         class="font-b612-mono-regular inline-block text-sm sm:text-base m-5 sm:m-7 bg-[#DCDFF5] text-[#333745] font-aldrich px-4 py-2 rounded-full hover:bg-[#949FF2] transition duration-500">
         Start Taking Pictures →
-      </button>
+    </a>
     </div>
 
     <img src={landing_frame} alt="Frame 1" class="m-auto h-fit max-w-full" />
