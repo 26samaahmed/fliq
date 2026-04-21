e662e3b5f4a1cd2a6fc41894d37b8ed9be8a8fdb
Ashley Chan <ashleyc417@csu.fullerton.edu>
2026-04-21 12:41:04 -0700

    Adjust spacing & buttons for mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,18 +10,18 @@
   <div class="flex-1">
     <div class="text-center mt-8">
       <h1 class="font-aldrich text-2xl sm:text-4xl text-white">Welcome!</h1>
-      <p class="font-aldrich tracking-tight text-base sm:text-lg text-[#E8F1F2] mt-5 max-w-4xl mx-auto">
+      <p class="font-aldrich tracking-tight text-xs sm:text-lg text-[#E8F1F2] mt-5 max-w-4xl mx-auto">
         Create and customize photo memories with your friends in real time as fliq. brings the photo booth experience to you wherever you are.
       </p>
-      
+
       <a
         href="/step1"
-        class="font-aldrich text-sm md:text-base inline-block m-5 sm:m-7 bg-[#D38A8A] text-white font-aldrich px-6 py-1 md:px-8 md:py-2 rounded-lg border-2 border-white hover:bg-[#C07070] transition duration-300">
+        class="font-aldrich text-sm md:text-base inline-block mt-8 mb-5 sm:m-7 bg-[#D38A8A] text-white font-aldrich px-6 py-1 md:px-8 md:py-2 rounded-lg border-2 border-white hover:bg-[#C07070] transition duration-300">
         Start Taking Pictures →
     </a>
     </div>
 
-    <img src={landing_frame} alt="Frame 1" class="m-auto h-fit max-w-full" />
+    <img src={landing_frame} alt="Frame 1" class="m-auto mt-6 sm:mt-0 h-fit max-w-full" />
   </div>
 
   <Footer />
