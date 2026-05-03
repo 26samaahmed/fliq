3d95ef97298f0a7187d18fe5330afa747489a65c
Kate Lee <145916607+kaitlynhlee@users.noreply.github.com>
2026-05-02 17:27:33 -0700

    d

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const io = new Server(server, {
 const rooms = {}
 const roomHosts = {}
 const MAX_USERS = 2
+const roomSockets = {}
 
 // FORMER CAMERA VIEW LOGIC USING EJS
 
@@ -50,38 +51,37 @@ io.on('connection', socket => {
     });
 
     socket.on('join-room', (roomID, userID, isCreator) => {
-        if (!rooms[roomID]){
+        if (!rooms[roomID]) {
             rooms[roomID] = new Set()
-            roomHosts[roomID] = null
         }
 
-        if (isCreator) {
-            roomHosts[roomID] = socket.id
+        if (isCreator && !roomHosts[roomID]) {
+            roomHosts[roomID] = userID
         }
+        
+        roomSockets[userID] = socket.id
 
         if (rooms[roomID].size >= MAX_USERS) {
             socket.emit('room-full')
             return
         }
 
-        rooms[roomID].add(socket.id)
+        rooms[roomID].add(userID)
         socket.join(roomID)
 
-        if (roomHosts[roomID] === socket.id) {
+        if (roomHosts[roomID] === userID) {
             socket.emit('is-host')
         }
 
         socket.to(roomID).emit('user-connected', userID)
 
         socket.on('disconnect', () => {
-            rooms[roomID]?.delete(socket.id)
-            if (rooms[roomID]?.size === 0){
+            rooms[roomID]?.delete(userID)
+            delete roomSockets[userID]
+
+            if (rooms[roomID]?.size === 0) {
                 delete rooms[roomID]
                 delete roomHosts[roomID]
-            } else if (roomHosts[roomID] === socket.id) {
-                const nextHost = rooms[roomID].values().next().value
-                roomHosts[roomID] = nextHost
-                io.to(nextHost).emit('is-host')
             }
             socket.to(roomID).emit('user-disconnected', userID)
         })
