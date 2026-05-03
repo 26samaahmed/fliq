1903441e4d691bde1ee2fd72a3afefb9f3efc8f5
Kate Lee <145916607+kaitlynhlee@users.noreply.github.com>
2026-05-02 17:08:45 -0700

    c

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const adapter = process.env.VERCEL ? adapterVercel() : adapterNode();
 
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      fallback: 'handler.js'
-    })
+    adapter
   }
 };
 
