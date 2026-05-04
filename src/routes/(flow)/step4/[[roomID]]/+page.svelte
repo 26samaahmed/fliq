7cad65714566e801ebe1dd90fdfd90ea4a1b3def
kaillora <nathankai09@gmail.com>
2026-05-04 15:11:18 -0700

    Update +page.svelte

--- src/routes/(flow)/step4/[[roomID]]/+page.svelte
+++ src/routes/(flow)/step4/[[roomID]]/+page.svelte
@@ -286,7 +286,7 @@
 
 		{#if isCapturing}
 			<p class="text-center text-white/80 text-base sm:text-lg mt-4 max-w-2xl mx-auto">
-				Photo {photoCount + 1} of {TOTAL_PHOTOS}
+				Photo {photoCount} of {TOTAL_PHOTOS}
 			</p>
 		{:else}
 			<p class="text-center text-white/80 text-base sm:text-lg mt-4 max-w-2xl mx-auto">
